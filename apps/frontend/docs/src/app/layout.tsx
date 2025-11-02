dc92d4af172694efd51d4238f3f182f02ae59e88
Thiago Arruda <thiago_arruda@id.uff.br>
2025-11-02 15:15:21 -0300

    feat(frontend/docs): :sparkles: sample page with a diagram renders correctly

--- apps/frontend/docs/src/app/layout.tsx
+++ apps/frontend/docs/src/app/layout.tsx
@@ -8,14 +8,13 @@ export const metadata = {
   // Define your metadata here
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 };
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>;
 const navbar = (
   <Navbar
-    logo={<b>Nextra</b>}
+    logo={<b>Curator</b>}
   // ... Your additional navbar options
   />
 );
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>;
+const footer = <Footer>{new Date().getFullYear()} © Curator.</Footer>;
 
 export default async function RootLayout({
   children,
@@ -38,7 +37,6 @@ export default async function RootLayout({
       </Head>
       <body>
         <Layout
-          banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
--- apps/frontend/docs/src/mdx-components.js
+++ apps/frontend/docs/src/mdx-components.js
@@ -1,7 +1,7 @@
 import { useMDXComponents as getThemeComponents } from "nextra-theme-docs"; // nextra-theme-blog or your custom theme
 
 // Get the default MDX components
-const themeComponents = getThemeComponents();
+const themeComponents = getThemeComponents({});
 
 // Merge components
 export function useMDXComponents(components) {
