20555cd470e6627ac02a4e77c83197f0cc9621bc
Thiago Arruda <thiago_arruda@id.uff.br>
2025-11-08 19:45:23 -0300

    feat(infrastructure): :sparkles: Vercel landing page configuration

--- infrastructure/live/production/main.tf
+++ infrastructure/live/production/main.tf
@@ -1,4 +1,6 @@
-# 1. Read OUTPUTS from "global" 
+# infrastructure/live/production/main.tf
+
+# 1. Read OUTPUTS from "global"
 # Getting the ARN from the certificate
 data "terraform_remote_state" "global" {
   backend = "s3"
@@ -9,13 +11,13 @@ data "terraform_remote_state" "global" {
   }
 }
 
-# 2. Call docs_site module
+# 2. Call docs_site module (Infra AWS)
 module "docs_site" {
-  source = "../../modules/aws-static-site"
+  source = "../../modules/aws-static-site" # Usando o seu caminho de módulo
 
   # --- Specific values for this site ---
   domain_name           = "docs.curator.com.br"
-  github_org_repo       = "thiagofons/curator" # (Confirme se está correto)
+  github_org_repo       = "thiagofons/curator"
   github_branch         = "main"
 
   # --- Values read from "global" ---
@@ -23,26 +25,50 @@ module "docs_site" {
   oidc_provider_arn     = data.terraform_remote_state.global.outputs.github_oidc_provider_arn
 }
 
-# Finds the Hosted Zone (que já configuramos no /global)
+# 3. DNS Configuration
+# Finds our Hosted Zone (declared once, used by all)
 data "aws_route53_zone" "primary" {
   name = "curator.com.br"
 }
 
-# Creates the "Address" 
+# DNS Record for AWS Docs Site (CloudFront)
 resource "aws_route53_record" "docs_site_dns" {
   zone_id = data.aws_route53_zone.primary.zone_id
-  name    = module.docs_site.domain_name 
+  name    = module.docs_site.domain_name
   type    = "A"
 
   alias {
-    # Pois to our front door at Cloudfront
     name                   = module.docs_site.cloudfront_distribution_domain_name
     zone_id                = module.docs_site.cloudfront_distribution_hosted_zone_id
     evaluate_target_health = false
   }
 }
 
-# 3. Outputs for the PIPELINE (What we need for GitHub)
+# --- Vercel records ---
+
+# DNS Record for Vercel Landing Page (Root Domain)
+resource "aws_route53_record" "landing_page_root" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "curator.com.br"
+  type    = "A"
+  ttl     = 300
+  
+  records = ["216.198.79.1"]
+}
+
+# DNS Record for Vercel Landing Page (WWW Subdomain)
+resource "aws_route53_record" "landing_page_www" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "www.curator.com.br"
+  type    = "CNAME"
+  ttl     = 300
+  
+  records = ["1d77852cc516a0f1.vercel-dns-017.com."]
+}
+
+# -------------------------------------------
+
+# 4. Outputs for the PIPELINE (What we need for GitHub)
 output "docs_site_cloudfront_id" {
   description = "Cloudfront distribution ID for the pipeline"
   value       = module.docs_site.cloudfront_distribution_id
