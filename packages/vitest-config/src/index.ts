e04480df4669052d4585f1e30431c0c0650a5590
Thiago Arruda <thiago_arruda@id.uff.br>
2025-11-29 17:20:36 -0300

    feat(monorepo): :sparkles: Integrate SonarQube for static analysis, configure Vitest to generate JUnit and LCOV reports, and add related Docker Compose profiles and scripts

--- packages/vitest-config/src/index.ts
+++ packages/vitest-config/src/index.ts
@@ -1,9 +1,35 @@
 export const sharedConfig = {
   test: {
     globals: true,
-    environment: "jsdom",
+    reporters: ["default", "junit"],
+    outputFile: {
+      junit: "./test-report.xml",
+    },
     passWithNoTests: true,
     decoratorMetadata: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov"],
+      reportsDirectory: "./coverage",
+      exclude: [
+        "**/node_modules/**",
+        "**/dist/**",
+        "**/*.module.ts",
+        "**/*.dto.ts",
+        "**/*.entity.ts",
+        "**/main.ts",
+        "**/*.config.ts",
+        "**/index.ts",
+        "**/*.spec.ts",
+        "**/*.test.ts",
+      ],
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
+    },
     include: ["**/*.spec.ts", "**/*.it.spec.ts"],
     exclude: ["**/node_modules/**", "**/.git/**"],
   },
