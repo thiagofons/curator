9f4c313302e872b37a808a0de7ebb7d6134be900
Benito Pedro <benitopedro21@outlook.com>
2025-11-30 14:33:39 -0300

    feat: externalize server packages in Next.js config, update JSX transform to `react-jsx`, and include dev types in tsconfig.

--- apps/frontend/blog-cms/next.config.mjs
+++ apps/frontend/blog-cms/next.config.mjs
@@ -1,31 +1,26 @@
-import { withPayload } from "@payloadcms/next/withPayload"
+import { withPayload } from "@payloadcms/next/withPayload";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
   output: "standalone",
+  serverExternalPackages: [
+    "pino",
+    "esbuild",
+    "drizzle-kit",
+    "@payloadcms/db-postgres",
+  ],
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
-      ".cjs": [
-        ".cts",
-        ".cjs",
-      ],
-      ".js": [
-        ".ts",
-        ".tsx",
-        ".js",
-        ".jsx",
-      ],
-      ".mjs": [
-        ".mts",
-        ".mjs",
-      ],
-    }
+      ".cjs": [".cts", ".cjs"],
+      ".js": [".ts", ".tsx", ".js", ".jsx"],
+      ".mjs": [".mts", ".mjs"],
+    };
 
-    return webpackConfig
+    return webpackConfig;
   },
-}
+};
 
 export default withPayload(nextConfig, {
   devBundleServerPackages: false,
-})
+});
