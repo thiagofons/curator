ff6f4fa29792b66fefcbd3d27b2bb7f931fbf377
Thiago Arruda <thiago_arruda@id.uff.br>
2026-01-19 15:37:15 -0300

    fix(frontend/website): :bug: diabling unused buttons

--- packages/ui-web/src/components/base/button.tsx
+++ packages/ui-web/src/components/base/button.tsx
@@ -5,15 +5,14 @@ import * as React from "react";
 import { cn } from "../../lib/utils";
 
 const buttonVariants = cva(
-  "font-medium inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-lg ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 text-btn-text",
+  "font-medium inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-lg ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 text-btn-text",
   {
     variants: {
       variant: {
         default: "bg-primary text-white hover:bg-primary/90 shadow-sm",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        outline: "border border-input bg-background hover:bg-accent ",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
