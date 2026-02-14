2c54b7f0eca310ab6742214530777b4d9b6b8fed
Thiago Arruda <thiago_arruda@id.uff.br>
2026-02-14 09:45:35 -0300

    feat(packages/model): :sparkles: creation of packages/model

---
+++ packages/model/vitest.config.ts
@@ -0,0 +1,8 @@
+import { sharedConfig } from "@repo/vitest-config";
+import { mergeConfig } from "vitest/config";
+
+export default mergeConfig(sharedConfig, {
+  test: {
+    environment: "node",
+  },
+});
