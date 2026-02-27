3aa3bb6593bf8c1eed902c172390d02f5cbb533c
Thiago Arruda <thiago_arruda@id.uff.br>
2026-02-27 12:11:26 -0300

    fix(monorepo): :bug: removing flags from init.sql

--- init.sql
+++ init.sql
@@ -4,25 +4,20 @@ CREATE EXTENSION IF NOT EXISTS "hstore";
 -- 2. Criação dos Usuários (Sem aspas = case-insensitive)
 CREATE ROLE api_user LOGIN PASSWORD 'api_password';
 CREATE ROLE cms_user LOGIN PASSWORD 'cms_password';
-CREATE ROLE flags_user LOGIN PASSWORD 'flags_password';
 
 -- 3. Criação dos Schemas
 CREATE SCHEMA api;
 CREATE SCHEMA cms;
-CREATE SCHEMA flags;
 
 -- 4. Atribuição de Donos (Crucial para automação de migrations)
 ALTER SCHEMA api OWNER TO api_user;
 ALTER SCHEMA cms OWNER TO cms_user;
-ALTER SCHEMA flags OWNER TO flags_user;
 
 -- 5. Configuração de Busca Automática (Search Path)
 ALTER ROLE api_user SET search_path TO api, public;
 ALTER ROLE cms_user SET search_path TO cms, public;
-ALTER ROLE flags_user SET search_path TO flags, public;
 
 -- 6. Permissões de Banco e Schema
 GRANT ALL ON SCHEMA api TO api_user;
 GRANT ALL ON SCHEMA cms TO cms_user;
 GRANT ALL ON SCHEMA public TO cms_user;
-GRANT ALL ON SCHEMA flags TO flags_user;
